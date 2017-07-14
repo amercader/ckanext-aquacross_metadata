8b89e70e08f13943e37e996316bc7d1ed65861ab
Declan Dunne <d.dunne@ucc.ie>
2017-07-14 14:17:29 +0200

    Add jQuery UI datepicker support for date input fields in the metadata editor

---
+++ ckanext/aquacross_metadata/fanstatic/aquacross_metadata_datepicker.js
@@ -0,0 +1,20 @@
+"use strict";
+
+ckan.module('aquacross_metadata_datepicker', function ($, _) {
+  return {
+    initialize: function () {
+      $.proxyAll(this, /_on/);
+
+      var data_field_id = this.options.id;
+      var data_field_id_selector = '#' + data_field_id;
+      var current_date = $(data_field_id_selector).val();
+      
+      $(data_field_id_selector).datepicker();
+      $(data_field_id_selector).datepicker( "option", "dateFormat", "yy-mm-dd" );
+      $(data_field_id_selector).datepicker( "option", "changeMonth", true );
+      $(data_field_id_selector).datepicker( "option", "changeYear", true );
+      $(data_field_id_selector).datepicker( "option", "yearRange", "1900:c");
+      $(data_field_id_selector).datepicker( "setDate" , current_date );
+    },
+  };
+});
